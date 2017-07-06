534f9b8e19bfd96449b70d50a62304dd4eaa707a
Taki Koutsomitis <koutst@protonmail.com>
2017-07-06 17:24:52 -0400

    render plane

---
+++ map_visualization.js
@@ -0,0 +1,52 @@
+(function(){
+  var margin = { top: 50, left: 50, right: 50, down: 50},
+    height = 400 - margin.top - margin.down,
+    width = 800 - margin.left - margin.right;
+    // debugger
+  var svg = d3.select("#map")
+    .append("svg")
+    .attr("height", height + margin.top + margin.down)
+    .attr("width", width + margin.left + margin.right)
+    .append("g")
+    .attr("transform", "translate("+ margin.left + "," + margin.top +")");
+    // debugger
+    d3.queue()
+      .defer(d3.json, "countries.topojson")
+      .await(ready);
+
+
+    var projection = d3.geoMercator()
+      .translate([ width / 2, height / 2 ])
+      .scale(100);
+
+
+    var path = d3.geoPath()
+      .projection(projection);
+
+
+    var plane = svg.append("path")
+                   .attr("class", "plane")
+                   .attr("cx", 60)
+                   .attr("cy", 60)
+                   .attr("r", 20)
+
+                       debugger
+    function ready(error, data) {
+      // console.log(data)
+      var countries = topojson.feature(data, data.objects.countries).features;
+      // debugger
+      console.log(countries);
+
+      svg.selectAll(".country")
+        .data(countries)
+        .enter()
+        .append("path")
+        .attr("class", "country")
+        .attr("d", path);
+        // debugger
+      svg.select(".plane")
+        .append("svg")
+        .attr("width", 200)
+        .attr("height", 200);
+    }
+})();
