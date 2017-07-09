bd95731234b84b766a765be3171aeee92e564f72
Taki Koutsomitis <koutst@protonmail.com>
2017-07-09 13:47:35 -0400

    add gitignore

--- map_visualization.js
+++ map_visualization.js
@@ -2,38 +2,31 @@
   var margin = { top: 50, left: 50, right: 50, down: 50},
     height = 400 - margin.top - margin.down,
     width = 800 - margin.left - margin.right;
-    // debugger
+
   var svg = d3.select("#map")
     .append("svg")
     .attr("height", height + margin.top + margin.down)
     .attr("width", width + margin.left + margin.right)
     .append("g")
     .attr("transform", "translate("+ margin.left + "," + margin.top +")");
-    // debugger
+
     d3.queue()
       .defer(d3.json, "countries.topojson")
       .defer(d3.json, "airports.topojson")
       .defer(d3.csv, "flights.csv")
       .await(ready);
 
-
     var projection = d3.geoMercator()
       .translate([ width / 2, height / 2 ])
       .scale(100);
-
 
     var path = d3.geoPath()
       .projection(projection);
 
 
-
-
-
     function ready(error, countriesData, airportsData, flightData) {
       console.log(flightData)
       var countriesParsed = topojson.feature(countriesData, countriesData.objects.countries).features;
-      // debugger
-      // console.log(countries);
 
       svg.selectAll(".country")
         .data(countriesParsed)
@@ -42,6 +35,10 @@
         .attr("class", "country")
         .attr("d", path)
         .on("mouseover", function(d){
+          d3.select(this)
+          .attr("class", "activeCountry")
+          .attr("fill", "black");
+
           d3.select(this.parentNode.parentNode.parentNode.parentNode)
           .append("text")
           .attr("class", "countrydetails")
@@ -60,10 +57,11 @@
           .text(
             "GDP (in millions): " + d.properties.GDP_MD_EST
           )
-          // debugger
         })
         .on("mouseout", function(d){
           d3.selectAll("text.countrydetails").remove()
+          d3.selectAll(".activeCountry")
+          .attr("class", "country")
         });
 
 
@@ -84,8 +82,10 @@
       for (var i = 0; i < flightData.length; i++) {
         orgAir = flightData[i]["ORIGIN"];
         destAir = flightData[i]["DEST"];
-        if (airportsNameCoord[orgAir] && airportsNameCoord[destAir]) {
-          flightData[i]["ORGCOORD"] = airportsNameCoord[orgAir]["coord"];
+        depTime = flightData[i]["DEP_TIME"]
+        if (airportsNameCoord[orgAir] && airportsNameCoord[destAir] && depTime) {
+          flightData[i]["type"] = "Point";
+          flightData[i]["coordinates"] = airportsNameCoord[orgAir]["coord"];
           flightData[i]["DESTCOORD"] = airportsNameCoord[destAir]["coord"];
           flights.push(flightData[i])
         }
@@ -94,7 +94,7 @@
       function transition(rect, route) {
         rect.attr('opacity', 1)
         .transition()
-        .duration(5000)
+        .duration(1000)
         .attrTween("transform", translateAttr(route.node()))
         .attr("d", path.pointRadius(0))
         .remove()
@@ -102,45 +102,72 @@
       }
 
       function translateAttr(path) {
-        // debugger
         let l = path.getTotalLength();
         return function(i) {
           return function(t) {
-            // debugger
             let p = path.getPointAtLength(t * l);
-            return "translate(" + (p.x-357.3425386309954) + "," + (p.y-50.425115551309283) + ")";
+            return "translate(" + (p.x-220) + "," + (p.y-73) + ")";
           }
         }
       }
 
+      let timer = 0;
 
-      for (var i = 0; i < flights.length-1; i++) {
-        let route = svg.append("path")
-          .data([{type: "LineString", coordinates: [flights[i].ORGCOORD, flights[i].DESTCOORD]}])
-          .attr("class", "route")
-          .attr("d", path)
-          .attr('opacity', .5)
-          .attr('fill', "none")
-          .attr('stroke', '	#00FF00')
-          .transition()
-          .duration(5000)
-          .remove();
-          // debugger
+      time = setInterval(function(){
 
-        let rect = svg.selectAll(".rect")
-        .data([airports[0]])
-        .enter()
-        .append("path")
-        .attr("class", "rect")
-        .attr("fill", "red")
-        .attr("d", path)
-        .attr("stroke-width", 0.5)
+        console.log(timer)
+        for (var i = 0; i < flights.length-1; i++) {
+          if (parseInt(flights[i].DEP_TIME) === timer) {
+            let route = svg
+            .data([{type: "LineString", coordinates: [flights[i].coordinates, flights[i].DESTCOORD]}])
+            .append("path")
+            .attr("class", "route")
+            .attr("d", path)
+            .attr('opacity', 0.5)
+            .attr('fill', "none")
+            .attr('stroke-width', 0.01)
+            .attr('stroke', '	#00FF00')
+            .transition()
+            .duration(1000)
+            .remove();
 
+            let rect = svg
+            .data([flights[i]])
+            .append("path")
+            .attr("class", "rect")
+            .attr("fill", "red")
+            .attr("d", path)
+            .attr("stroke-width", 0.5)
 
-        // transition(rect, route);
-      }
-      // var totalLength = path.node().getTotalLength();
+            transition(svg
+            .data([flights[i]])
+            .append("path")
+            .attr("class", "rect")
+            .attr("fill", "red")
+            .attr("d", path)
+            .attr("stroke-width", 0.5), svg
+            .data([{type: "LineString", coordinates: [flights[i].coordinates, flights[i].DESTCOORD]}])
+            .append("path")
+            .attr("class", "route")
+            .attr("d", path)
+            .attr('opacity', 0.5)
+            .attr('fill', "none")
+            .attr('stroke-width', 0.01)
+            .attr('stroke', '	#00FF00')
+            .transition()
+            .duration(1000)
+            .remove());
+          }
 
+        }
+        timer += 1
+        if (timer%100 >= 60) {
+          timer += 100 - timer%100
+        }
+        if (timer > 2400) {
+          clearInterval(time);
+        }
+      }, 100)
 
       svg.selectAll(".airport")
       .data(airports)
@@ -151,6 +178,13 @@
       .attr("scale", 100)
       .on("mouseover", function(d){
         // debugger
+        d3.select(this)
+        .attr("class", "activeAirport")
+        .attr("stroke", "blue")
+        .attr("stroke-width", 0.5)
+        .attr("fill", "red")
+        .attr("d", path.pointRadius(5));
+
         d3.select(this.parentNode.parentNode.parentNode.parentNode)
         .append("text")
         .attr("class", "countrydetails")
@@ -167,6 +201,11 @@
       })
       .on("mouseout", function(d){
         d3.selectAll("text.countrydetails").remove()
+
+        d3.selectAll(".activeAirport")
+        .attr("class", "airport")
+        .attr("d", path.pointRadius(1.5));
+
       });
 
 
