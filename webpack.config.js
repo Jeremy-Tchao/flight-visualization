2e6d558c8c7964d3000de5dc9e58c4b28d0342c5
Taki Koutsomitis <koutst@protonmail.com>
2017-07-16 16:44:02 -0400

    Moved to new directory

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+    entry: './lib/map_visualization.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'html-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
