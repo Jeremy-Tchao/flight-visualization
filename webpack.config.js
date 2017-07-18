1b34ceb906ae0a52c88d3eb8a588768b95396a14
Taki Koutsomitis <koutst@protonmail.com>
2017-07-18 11:47:08 -0400

    added some npm

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                loader: 'html-loader',
+                loader: 'babel-loader',
                 query: {
                     presets: ['es2015']
                 }
